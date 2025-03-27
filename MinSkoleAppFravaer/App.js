b7ffef39def294c3a853c29860a12fdce753f542
KasperL22R <119696654+Repsak05@users.noreply.github.com>
2025-03-27 13:16:53 +0100

    Buzzer & A bit of ReactNative

--- MinSkoleAppFravaer/App.js
+++ MinSkoleAppFravaer/App.js
@@ -1,10 +1,38 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+
+  const [newId, setNewId] = useState("");
+  const [theId, setTheId] = useState("");
+
+  function addId(id){
+    setTheId(id);
+    console.log("Adding", id, "to DB");
+    //add value to db
+  }
+
   return (
     <View>
       <Text>WOW - MinSkoleApp</Text>
+      
+      <TextInput
+        onChangeText={(text) => setNewId(text)}
+        value={newId} 
+        placeholder="Type ID to add..."
+      />
+
+      <Button
+        onPress = {() => addId(newId)}
+        title = "Tilføj ID"
+      />
+
+      <Text> Current id's</Text>
+      {theId && (
+        <Text>{theId}</Text>
+      )}
+
       <StatusBar style="auto" />
     </View>
   );
--- RFID_Reader/arduinoRFID/arduinoRFID.ino
+++ RFID_Reader/arduinoRFID/arduinoRFID.ino
@@ -5,6 +5,8 @@
 #define SS_PIN 10
 #define RST_PIN 9
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+
+const int buzzer = 8; //buzzer to arduino pin 9
  
 void setup() 
 {
@@ -13,17 +15,24 @@ void setup()
   mfrc522.PCD_Init();   // Initiate MFRC522
   // Serial.println("Approximate your card to the reader...");
   // Serial.println(2);
+  pinMode(buzzer, OUTPUT);
 }
 void loop() 
 {
   String ID = getId();
   if(ID.length()){
     Serial.println(ID);
-    delay(3000);
+
+    //Set buzzer active (Might want to get signal from .py to ensure that it worked)
+    tone(buzzer, 1000); // Send 1KHz sound signal...
+    delay(500);         // ...for 1 sec
+    noTone(buzzer);     // Stop sound...
+    delay(2000);         
   }
 } 
 
-String getId(){
+String getId()
+{
    // Look for new cards
   if ( ! mfrc522.PICC_IsNewCardPresent()) {
     return "";
@@ -39,8 +48,6 @@ String getId(){
   byte letter;
   for (byte i = 0; i < mfrc522.uid.size; i++) 
   {
-    // Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-    // Serial.print(mfrc522.uid.uidByte[i], HEX);
     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
     content.concat(String(mfrc522.uid.uidByte[i], HEX));
   }
